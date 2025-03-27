a8210813fde8eec304634c960c7ea4a4a6ff5c79
Helene Harrer <if22b036@technikum-wien.at>
2025-03-28 00:48:02 +0100

    vite

--- frontend-adaptive/vite.config.js
+++ frontend-adaptive/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
+    host: true, // erlaubt Zugriff von außen
+    strictPort: true,
+    allowedHosts: [
+      "frontend-adaptive.onrender.com",
+      "localhost"
+    ]
   }
 })
--- frontend-responsive/vite.config.js
+++ frontend-responsive/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: 3000,
+    host: true, // erlaubt Zugriff von außen
+    strictPort: true,
+    allowedHosts: [
+      "frontend-responsive-bp8j.onrender.com",
+      "localhost"
+    ]
   }
 })
